b4a59fe77d767f0b0b80792663430bba396aedb7
MatthewP-Newbee <matthewhakka@gmail.com>
2019-03-10 16:36:29 +0800

    Update

---
+++ pathos/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+# @Project: Python_Learning
+# @Date: 19-3-10
+# @Author: MatthewP
---
+++ pathos/amap_example.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+# @Project: Python_Learning
+# @Date: 19-3-10
+# @Author: MatthewP
+
+# import time
+# from pathos.pp import ParallelPool
+# pool = ParallelPool(6)
+#
+# results = pool.amap(pow, [1, 2, 3, 4], [5, 6, 7, 8])
+# while not results.ready():
+#     time.sleep(3)
+#     print('.', end=' ')
+#
+# print(results.get())
+
+import pathos
+print(dir(pathos))
