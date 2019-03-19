19f80e04c3b35955c6862a2f4e815e1b92d44aea
MatthewP-Newbee <matthewhakka@gmail.com>
2019-03-19 13:58:40 +0800

    Update

--- pathos/amap_example.py
+++ pathos/amap_example.py
@@ -4,16 +4,14 @@
 # @Date: 19-3-10
 # @Author: MatthewP
 
-# import time
-# from pathos.pp import ParallelPool
-# pool = ParallelPool(6)
-#
-# results = pool.amap(pow, [1, 2, 3, 4], [5, 6, 7, 8])
-# while not results.ready():
-#     time.sleep(3)
-#     print('.', end=' ')
-#
-# print(results.get())
+import time
+from pathos.pp import ParallelPool
+pool = ParallelPool(6)
 
-import pathos
-print(dir(pathos))
+results = pool.amap(pow, [1, 2, 3, 4], [5, 6, 7, 8])
+while not results.ready():
+    time.sleep(3)
+    print('.', end=' ')
+
+print(results.get())
+
--- pytest/test_capitalize.py
+++ pytest/test_capitalize.py
@@ -5,8 +5,9 @@
 # @Author: MatthewP
 
 
-def capital_case(x: str):
+def capital_case(x):
     return x.capitalize()
+
 
 def test_capital_case():
     assert capital_case('semaphore') == 'Semaphore'
